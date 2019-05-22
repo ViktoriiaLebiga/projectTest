11fd6a594f7e43efd2e1824f247d4da9357b5cb3
Viktoriia <vikusia.lebiga@gmail.com>
2019-05-22 22:34:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //1
+        System.out.print("Hello world!\n");
+        //2
+        System.out.println("Hello world!");
+        //3
+        String str = "Hello world!";
+        System.out.println(str);
+        //4
+        String h = "Hello";
+        String w = "world!";
+        System.out.println(h + " " + w);
+        //5
+        String[] t = new String[]{"Hello", "world", "!"};
+        System.out.println(t[0] + " " + t[1] + t[2]);
+    }
+}
